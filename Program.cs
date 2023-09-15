3f075e63f566aab78cd9f78d80d054cd1251c8ea
Creation's <creations@creations.works>
2023-09-15 04:24:15 -0400

    misspelled my username lmao

--- Program.cs
+++ Program.cs
@@ -436,7 +436,7 @@ class Program
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("Disclaimer: this was forked from https://github.com/larsl2005/StandConsoleInjector");
         Console.WriteLine("Im not sure if injecting stand this way is safe so use at your own risk");
-        Console.WriteLine("If you have any issues with this fork make a issue on github or dm me on discord @ Creatons\n\n");
+        Console.WriteLine("If you have any issues with this fork make a issue on github or dm me on discord @ Creations\n\n");
         Console.WriteLine("Known problems:");
         Console.WriteLine("> injecting before the game has fully loaded will crash your game");
         Console.WriteLine("\nPress any key to continue...");
